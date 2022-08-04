9d8d88f4b89f8afe18ccf1e0ec387c5316fc4911
Anjali Kumari Dubey <AKDdubeyanjali9819@gmail.com>
2022-08-05 00:10:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import Todo from "./components/Todo";
 import InputTodo from "./components/InputTodo";
 
 function App() {
   return <div>
     <h1>My Todos</h1>
     <InputTodo />
-    <Todo text='Learn React'/>
-    <Todo text='Master React' />
-    <Todo text='Explore React More' />
   </div>;
 }
 
