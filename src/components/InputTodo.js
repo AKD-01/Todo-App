cff3acc3c1701e9adc517140ffa920f03c857675
Anjali Kumari Dubey <AKDdubeyanjali9819@gmail.com>
2022-08-05 00:10:12 +0530

    Update InputTodo.js

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,9 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect } from 'react';
 import Todo from './Todo';
 
 const InputTodo = () => {
     const [value, setValue] = useState("");
     const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        if(localStorage.getItem("localTodo")){
+            const storedTodos = JSON.parse(localStorage.getItem("localTodo"));
+            setTodos(storedTodos);
+        }
+    },[]);
 
     const inputChangeHandler = (e) => {
         setValue(e.target.value);
@@ -16,8 +23,15 @@ const InputTodo = () => {
     };
 
     const addTodo = () => {
-        const newTodo = {text:value};
+        const newTodo = {text:value, id:new Date().getTime().toString()};
         setTodos([...todos,newTodo]);
+        localStorage.setItem("localTodo", JSON.stringify([...todos, newTodo]));
+    };
+
+    const deleteTodo = (todo) => {
+        const updated = todos.filter(t => t.id !== todo.id);
+        setTodos(updated);
+        localStorage.setItem("localTodo", JSON.stringify(updated));
     };
 
     return(
@@ -29,12 +43,19 @@ const InputTodo = () => {
         value={value} 
         onChange={inputChangeHandler} 
         />
+       
         <button type="submit" onClick={addTodo}>
             Add
         </button>
     </form>
+    <div>
+            You have
+            {
+                !todos.length ? " no todos" : todos.length===1 ? "1 todo" : todos.length > 1 ? ` ${todos.length} todos` : null
+            }
+        </div>
     <span>
-        {todos.map(todo => <Todo text={todo.text}/>)}; 
+        {todos.map(todo => <Todo key={todo.id} text={todo.text} deleteTodo={deleteTodo} todo={todo}/>)}; 
     </span>
     </>
     );
